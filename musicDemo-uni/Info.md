4e5231a63a41a8da10dcc5334ac7a6dc6ff755cc
sinian <1198246001@qq.com>
2021-12-17 17:42:10 +0800

    uni demo上传12/17

---
+++ musicDemo-uni/Info.md
@@ -0,0 +1,12 @@
+### 应用生命周期
+
+onLaunch onShow onHide
+
+### 页面声明周期
+
+onLoad onShow onReady onHide onUnload onResize 
+
+onPullDownRefresh onReachBottom onShareAppMessage 
+
+onPageScroll onBackPress
+
---
+++ musicDemo-uni/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<!-- 		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view> -->
+		<view :title="singer">喜欢的歌手是：{{singer}}</view>
+		<button @click="changeSingerName">更换名字</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				singer: '周杰伦'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			changeSingerName() {
+				const temp = this.singer
+				this.singer = temp[0] === "反" ? "周杰伦" : "反正不是周杰伦"
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
